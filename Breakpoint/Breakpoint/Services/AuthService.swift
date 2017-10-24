66a028db544446de3a5c1393d65c7c669f55608a
Mitchell Schuster <mgschuster11@gmail.com>
2017-10-24 17:38:24 +0300

    created class

---
+++ Breakpoint/Breakpoint/Services/AuthService.swift
@@ -0,0 +1,22 @@
+//
+//  AuthService.swift
+//  Breakpoint
+//
+//  Created by Admin on 10/24/17.
+//  Copyright © 2017 TJSchoost. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class AuthService {
+    static let instance = AuthService()
+    
+    func registerUser() {
+        
+    }
+    
+    func loginUser() {
+        
+    }
+}
