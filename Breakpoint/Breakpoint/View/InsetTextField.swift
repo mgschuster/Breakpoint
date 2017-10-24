781a6e54c117df1697086b6084142fdf40609f5f
Mitchell Schuster <mgschuster11@gmail.com>
2017-10-24 17:25:11 +0300

    textfield padding

---
+++ Breakpoint/Breakpoint/View/InsetTextField.swift
@@ -0,0 +1,27 @@
+//
+//  InsetTextField.swift
+//  Breakpoint
+//
+//  Created by Admin on 10/24/17.
+//  Copyright © 2017 TJSchoost. All rights reserved.
+//
+
+import UIKit
+
+class InsetTextField: UITextField {
+
+    private var padding = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 0)
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+
+}
