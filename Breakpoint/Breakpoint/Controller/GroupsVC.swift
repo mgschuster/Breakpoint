9fcbc08023a3e52474b9319e6990e4cc932a9905
Mitchell Schuster <mgschuster11@gmail.com>
2017-10-31 18:08:42 +0300

    completed added groups to tableview

--- Breakpoint/Breakpoint/Controller/GroupsVC.swift
+++ Breakpoint/Breakpoint/Controller/GroupsVC.swift
@@ -13,10 +13,23 @@ class GroupsVC: UIViewController {
     // Outlets
     @IBOutlet weak var groupsTableView: UITableView!
     
+    // Variables
+    var groupsArray = [Group]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         groupsTableView.delegate = self
         groupsTableView.dataSource = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        DataService.instance.REF_GROUPS.observe(.value) { (snapshot) in
+            DataService.instance.getAllGroups { (returnedGroupsArray) in
+                self.groupsArray = returnedGroupsArray
+                self.groupsTableView.reloadData()
+            }
+        }
     }
 }
 
@@ -26,12 +39,13 @@ extension GroupsVC: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return groupsArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = groupsTableView.dequeueReusableCell(withIdentifier: "groupCell", for: indexPath) as? GroupCell else { return UITableViewCell() }
-        cell.configureCell(title: "Nerd Herd", description: "The nerdiest nerds around.", memberCount: 3)
+        let group = groupsArray[indexPath.row]
+        cell.configureCell(title: group.groupTitle, description: group.groupDesc, memberCount: group.memberCount)
         return cell
     }
 }
