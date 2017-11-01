58b59d1b544a74ebb8b88f7c136c6e79c8b36c8a
Mitchell Schuster <mgschuster11@gmail.com>
2017-11-01 17:35:28 +0300

    loading all group data!

--- Breakpoint/Breakpoint/Controller/GroupFeedVC.swift
+++ Breakpoint/Breakpoint/Controller/GroupFeedVC.swift
@@ -13,13 +13,29 @@ class GroupFeedVC: UIViewController {
     // Outlets
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var groupTitleLbl: UILabel!
+    @IBOutlet weak var membersLbl: UILabel!
     @IBOutlet weak var sendBtnView: UIView!
     @IBOutlet weak var messageTextField: InsetTextField!
     @IBOutlet weak var sendBtn: UIButton!
     
+    // Variables
+    var group: Group?
+    
+    func initData(forGroup group: Group) {
+        self.group = group
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         sendBtnView.bindToKeyboard()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        groupTitleLbl.text = group?.groupTitle
+        DataService.instance.getEmailsFor(group: group!) { (returnedEmails) in
+            self.membersLbl.text = returnedEmails.joined(separator: ", ")
+        }
     }
     
     // Actions
